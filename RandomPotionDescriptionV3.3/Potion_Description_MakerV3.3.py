636e4cfc96c449090de2c65f2ccc1f14ba336c69
twelfthgear <43900344+twelfthgear@users.noreply.github.com>
2018-10-08 23:06:16 -0500

    Add files via upload

---
+++ RandomPotionDescriptionV3.3/Potion_Description_MakerV3.3.py
@@ -0,0 +1,86 @@
+
+import random
+import locale
+print ("Hello there!")
+print ("This program helps generate random potion decriptions!", '\n')
+
+def randomPot ():
+      potTitle = (open('potTitle.txt').readlines())
+      potEffect = (open('potEffect.txt').readlines())
+      container = (open('container.txt').readlines())
+      appearance1 = (open('appearance1.txt').readlines())
+      appearance2 = (open('appearance2.txt').readlines())
+      texture = (open('texture.txt').readlines())
+      label = (open('label.txt').readlines())
+      strength = (open('strength.txt').readlines())
+      sideEffect = (open('sideEffect.txt').readlines())
+      tasteSmell = (open('tasteSmell.txt').readlines())
+      
+      potNum = eval(input("Please enter how many potions you wish to generate: "))
+      print ()
+      for x in range(potNum):
+                  strength2 = random.choice(strength)
+                  potCost = random.randint(50, 500)
+                  GP = potCost
+                  SP = potCost * 10
+                  CP = potCost * 100
+                  
+                  print ("Type: \t\t",random.choice(potTitle))
+                  print ("Effect: \t","of ",random.choice(potEffect))
+                  print ("Strength: \t", strength2)
+                  if "Regular with no side effect." in (strength2):
+                        print ("Side Effects: \t No side effects.")
+                  elif "Seemingly permanent." in (strength2):
+                        print ("Side Effects: \t No side effects.")
+                  elif "Major with a strong side effect." in (strength2):
+                        print ("Side Effects: \t", random.choice(sideEffect), '\t\t', random.choice(sideEffect))
+                  elif "Minor with a strong side effect." in (strength2):
+                        print ("Side Effects: \t", random.choice(sideEffect), '\t\t', random.choice(sideEffect))
+                  elif "Regular with a strong side effect." in (strength2):
+                        print ("Side Effects: \t", random.choice(sideEffect), '\t\t', random.choice(sideEffect)) 
+                  elif "Regular with a slight side effect." in (strength2):
+                        print ("Side Effect: \t", random.choice(sideEffect))
+                  elif "Minor with a slight side effect." in (strength2):
+                        print ("Side Effect: \t", random.choice(sideEffect))
+                  elif "Major with a small side effect." in (strength2):
+                        print ("Side Effect: \t", random.choice(sideEffect))  
+                  elif "A poison. Almost no positive effect or side effect." in (strength2):
+                        print ("Side Effect: \t", random.choice(sideEffect))
+                  else:
+                        print ("Side Effects: \t", random.choice(sideEffect),
+                              '\t\t', random.choice(sideEffect), 
+                              '\t\t', random.choice(sideEffect))
+                  print ("Container: \t", random.choice(container))
+                  print ("Appearance: \t", random.choice(appearance1),
+                         '\t\t', "with", random.choice(appearance2),
+                         '\t\t', "When sloshed, it feels", random.choice(texture))
+                  print ("Label: \t\t", random.choice(label))
+                  print ("Taste: \t\t", random.choice(tasteSmell))
+                  print ("Smell: \t\t", random.choice(tasteSmell))
+                  print ("Cost: ", '\t\t',"GP: ",GP, '\n',
+                         '\t\t',"SP: ",SP, '\n',
+                         '\t\t',"CP: ",CP, '\n')
+                        
+                  print ('---------------------------------------------------------------------------------------', '\n')
+      if potNum == 1:
+            print ( "Here is your random potion! Enjoy!")
+      else:
+            ("Here are your", potNum, "random potions! Enjoy!")
+      print()
+      input("- Hit ENTER to exit the program! -")
+randomPot()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RandomPotionDescriptionV3.3/READ_ME.txt
@@ -0,0 +1,30 @@
+Hi! Welcome to twelfthGear's potion generator V3.X
+
+If you want to run this program, be sure to do the following:
+----------------------------------------------------------------------------
+1.) How I have the program set up, you need both the program file and the 
+    .txt files in the same folder in order for the program to run. 
+
+2.) If you do not have python, you can download it at https://bit.ly/1L2MyD2
+    
+    Just click Download Python 3.7.X and install accordingly.
+    
+3.) Once/if you have python installed, double 
+    clicking the program file to run it.
+
+4.) Once you have done the previous steps, you can sit back, follow
+    the prompt, and enjoy the automation.
+----------------------------------------------------------------------------
+
+Sources pulled from:
+	Reddit:
+		u/twelfthgear:
+			My Brain! 
+		u/dndspeak:
+			https://bit.ly/2OKBhBf
+
+	Elsewhere:
+			https://bit.ly/2PuNPtq
+
+	
+	
